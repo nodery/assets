be2930eeddfcc1193cdbf213e4ccbc230dd3f0aa
Richard King <richrdkng@gmail.com>
2020-04-17 20:11:10 +0200

    build: add SVG and PNG asset build scripts

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,68 @@
 const path = require('path')
 const gulp = require('gulp')
 const debug = require('gulp-debug')
+const imagemin = require('gulp-imagemin')
+const png = require('imagemin-pngquant')
 
 const root = path.resolve(__dirname, '../')
 
-gulp.task('build', async () => {
+const svgoPluginsConfig = [
+  { cleanupNumericValues: { floatPrecision: 0 } },
+  { convertPathData: { floatPrecision: 0 } },
+  { transformsWithOnePath: { floatPrecision: 0 } },
+  { convertTransform: { floatPrecision: 0 } },
+  { cleanupListOfValues: { floatPrecision: 0 } }
+]
+
+gulp.task('build:copy-files', async () => {
   gulp
     .src([
       `${root}/README*`,
-      `${root}/LICENSE*`,
-      `${root}/src/**/*.js`
+      `${root}/LICENSE*`
     ])
     .pipe(debug())
     .pipe(gulp.dest(`${root}/dist`))
 })
+
+gulp.task('build:logo', async () => {
+  gulp
+    .src(`${root}/src/logo/**/*.svg`)
+    .pipe(debug())
+    .pipe(imagemin([
+      imagemin.svgo({ plugins: svgoPluginsConfig })
+    ]))
+    .pipe(gulp.dest(`${root}/dist/logo`))
+})
+
+gulp.task('build:media:github:png', async () => {
+  gulp
+    .src(`${root}/src/media/github/**/*.png`)
+    .pipe(debug())
+    .pipe(imagemin([
+      png({
+        speed: 1,
+        strip: true,
+        quality: [0.75, 0.95]
+      })
+    ]))
+    .pipe(gulp.dest(`${root}/dist/media/github`))
+})
+
+gulp.task('build:media:github:svg', async () => {
+  gulp
+    .src(`${root}/src/media/github/**/*.svg`)
+    .pipe(debug())
+    .pipe(imagemin([
+      imagemin.svgo({ plugins: svgoPluginsConfig })
+    ]))
+    .pipe(gulp.dest(`${root}/dist/media/github`))
+})
+
+gulp.task('build',
+  gulp.parallel(
+    'build:copy-files',
+    'build:logo',
+    'build:media:github:png',
+    'build:media:github:svg'
+  )
+)
